65dcd6289140591bef901da0e8cf92deb26781ba
AlabamaSnail <74316013+AlabamaSnail@users.noreply.github.com>
2024-04-02 14:46:34 -0500

    Updated button_function()
    Changed the function

--- PresenceProctor/main.py
+++ PresenceProctor/main.py
@@ -7,7 +7,7 @@ app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
 
 def button_function():
-    print("button pressed")
+    print("Printed Button")
 
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
