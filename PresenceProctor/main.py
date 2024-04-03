2b121309eb9915d1900cd1c9878ee56ce6b7406e
AlabamaSnail <74316013+AlabamaSnail@users.noreply.github.com>
2024-04-03 15:00:12 -0500

    TEst
    Main.py change

--- PresenceProctor/main.py
+++ PresenceProctor/main.py
@@ -1,25 +1,44 @@
 import customtkinter
-
+import tkinter
+"""
 customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+app.geometry("1920x1080")
 
+
+# relx = X
+# rely = Y
 def button_function():
-    print("Printed Button abc123")
+    print("Pressed Button")
 
 
 optionmenu_var = customtkinter.StringVar(value="Dark")  # set initial value
 
+
+def button_click_event():
+    dialog = customtkinter.CTkInputDialog(text="Password:", title="Password Request")
+    print("Password:", dialog.get_input())
+
+
+Passbutton = customtkinter.CTkButton(app, text="Login", command=button_click_event)
+Passbutton.place(relx=0.05, rely=0.01, anchor=tkinter.N)
+
+
 def optionmenu_callback(choice):
-    print("optionmenu dropdown clicked:", choice)
+    if choice == "Dark":
+        customtkinter.set_appearance_mode("dark")
+    else:
+        customtkinter.set_appearance_mode("light")
 
 combobox = customtkinter.CTkOptionMenu(master=app,
                                        values=["Dark", "Light"],
                                        command=optionmenu_callback,
-                                       variable=optionmenu_var)
+                                       variable=optionmenu_var,
+                                       corner_radius=20)
 combobox.pack(padx=20, pady=10)
+
 
 
 # Use CTkButton instead of tkinter Button
@@ -27,3 +46,73 @@ button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_fu
 button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
 app.mainloop()
+"""
+
+import customtkinter
+from PIL import Image
+import os
+
+customtkinter.set_appearance_mode("dark")
+
+
+class App(customtkinter.CTk):
+    width = 900
+    height = 600
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.title("CustomTkinter example_background_image.py")
+        self.geometry(f"{self.width}x{self.height}")
+        self.resizable(False, False)
+
+        # load and create background image
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        self.bg_image = customtkinter.CTkImage(Image.open(current_path + "/test_images/bg_gradient.jpg"),
+                                               size=(self.width, self.height))
+        self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image)
+        self.bg_image_label.grid(row=0, column=0)
+
+        # create login frame
+        self.login_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.login_frame.grid(row=0, column=0, sticky="ns")
+        self.login_label = customtkinter.CTkLabel(self.login_frame, text="PresenseProctor AI\nLogin Page",
+                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.login_label.grid(row=0, column=0, padx=30, pady=(150, 15))
+
+        ## WORKING ON THIS LINE ##
+
+        self.login_Icon = customtkinter.CTkImage(Image.open(current_path + "/test_images/logo.png"), size=(self.width, self.height))
+
+        ##########################
+
+        self.username_entry = customtkinter.CTkEntry(self.login_frame, width=200, placeholder_text="username")
+        self.username_entry.grid(row=1, column=0, padx=30, pady=(15, 15))
+        self.password_entry = customtkinter.CTkEntry(self.login_frame, width=200, show="*", placeholder_text="password")
+        self.password_entry.grid(row=2, column=0, padx=30, pady=(0, 15))
+        self.login_button = customtkinter.CTkButton(self.login_frame, text="Login", command=self.login_event, width=200)
+        self.login_button.grid(row=3, column=0, padx=30, pady=(15, 15))
+
+        # create main frame
+        self.main_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.main_frame.grid_columnconfigure(0, weight=1)
+        self.main_label = customtkinter.CTkLabel(self.main_frame, text="PresenseProctor AI\nDashboard",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.main_label.grid(row=0, column=0, padx=30, pady=(30, 15))
+        self.back_button = customtkinter.CTkButton(self.main_frame, text="Back", command=self.back_event, width=200)
+        self.back_button.grid(row=1, column=0, padx=30, pady=(15, 15))
+
+    def login_event(self):
+        print("Login pressed - username:", self.username_entry.get(), "password:", self.password_entry.get())
+
+        self.login_frame.grid_forget()  # remove login frame
+        self.main_frame.grid(row=0, column=0, sticky="nsew", padx=100)  # show main frame
+
+    def back_event(self):
+        self.main_frame.grid_forget()  # remove main frame
+        self.login_frame.grid(row=0, column=0, sticky="ns")  # show login frame
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
