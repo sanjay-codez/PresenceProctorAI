5c75ac63f297072efc26151bcd3e51ddacfdb957
Alex <165716047+alextran0@users.noreply.github.com>
2024-04-04 14:37:47 -0500

    added new test py file

--- PresenceProctor/testfacerecog.py
+++ PresenceProctor/testfacerecog.py
@@ -2,7 +2,7 @@ import cv2
 from deepface import DeepFace
 import os
 from multiprocessing.dummy import Pool as ThreadPool
-
+#f
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
